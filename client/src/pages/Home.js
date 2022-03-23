f9650644771accba0243415cf9c1ca4346f5d104
Craig-UK <ScytherHQ@gmail.com>
2022-03-23 02:22:19 +0000

    Added the functionality to create posts on the frontend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import { Grid } from 'semantic-ui-react';
 
+import { AuthContext } from '../context/auth';
 import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
 
 function Home() {
-    const { loading, data } =useQuery(FETCH_POST_QUERY);
+    const { user } = useContext(AuthContext);
+    const { 
+        loading, 
+        data 
+    } = useQuery(FETCH_POST_QUERY);
 
     return (  
     <Grid columns={3}>
@@ -14,16 +20,21 @@ function Home() {
             <h1>Recent Posts</h1>
         </Grid.Row>
         <Grid.Row>
-        { loading ? (
-            <h1>Loading posts...</h1>
-        ) : (
-            data && 
-            data.getPosts.map ((post) => (
-                <Grid.Column key={post.id} style={{marginBottom: 100 }}>
-                    <PostCard post={post} />
+            { user && (
+                <Grid.Column>
+                    <PostForm />
                 </Grid.Column>
-            ))
-        )}
+            )}
+            { loading ? (
+                <h1>Loading posts...</h1>
+            ) : (
+                data && 
+                data.getPosts.map ((post) => (
+                    <Grid.Column key={post.id} style={{marginBottom: 100 }}>
+                        <PostCard post={post} />
+                    </Grid.Column>
+                ))
+            )}
     </Grid.Row>
     </Grid>
     );
