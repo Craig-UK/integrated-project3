fffb3c33118cb1c0b822b9530f7f8924e86343ba
= <=>
2022-03-26 19:05:46 +0000

    Single Post Page Operational Delete Button not Refreshing

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import MenuBar from './components/MenuBar';
 import Home from './pages/Home.js';
 import Register from './pages/Register.js';
 import Login from './pages/Login.js';
+import SinglePost from './pages/SinglePost.js';
 
 function App() {
   return (
@@ -23,7 +24,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <AuthRoute exact path="/login" component={Login} />
           <AuthRoute exact path="/register" component={Register} />
-
+          <Route exact path= "/posts/:postId" component={SinglePost}></Route>
         </Container>
       </Router>
     </AuthProvider>
--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton'
 
 function PostCard({post: { body, createdAt, id, username, likeCount, commentCount, likes}}){
   const { user } = useContext(AuthContext); 
@@ -19,14 +20,14 @@ function PostCard({post: { body, createdAt, id, username, likeCount, commentCoun
             src='https://react.semantic-ui.com/images/avatar/large/molly.png'
           />
           <Card.Header>{username}</Card.Header>
-          <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow(true)}</Card.Meta>
+          <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow(false)}</Card.Meta>
           <Card.Description>
             {body}
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
-      <LikeButton user={user} post={{ id, likes, likeCount }}/>
-    <Button labelPosition='right' as={Link} to={`/post/${id}`}>
+      <LikeButton user={user} post={{ id, likeCount, likes }}/>
+    <Button labelPosition='right' as={Link} to={`/posts/${id}`}>
       <Button color='orange'>
         <Icon name='comments' />
         Comments
@@ -35,12 +36,7 @@ function PostCard({post: { body, createdAt, id, username, likeCount, commentCoun
         {commentCount}
       </Label>
     </Button>
-    {user && user.username === username && (
-      <Button color='red' floated='right' onClick={() => console.log('Delete Post')}>
-        <Icon name="trash" style={{marginTop: 0}}/>
-        Delete
-      </Button>
-    ) }
+    {user && user.username === username && <DeleteButton postId={id} />}
         </Card.Content>
       </Card>
     )
